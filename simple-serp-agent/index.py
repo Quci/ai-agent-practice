9841eeef1b2dec3eeb1c3c4f57529ebc5e916277
Quci <24872191+Quci@users.noreply.github.com>
2025-03-06 02:15:41 +0800

    serp agent demo 代码优化

--- simple-serp-agent/index.py
+++ simple-serp-agent/index.py
@@ -1,20 +1,12 @@
 from dotenv import load_dotenv
-import os
 from langchain import hub
 from langchain_community.llms import OpenAI
 from langchain_community.utilities import SerpAPIWrapper
 from langchain.tools import Tool
 from langchain.agents import create_react_agent
 from langchain.agents import AgentExecutor
-import langchain  # 导入langchain模块
-
-# 启用全局调试模式
-# langchain.debug = True
 
 
-# os.environ["LANGCHAIN_PROJECT"] = 'agent-demo'
-
-# 加载环境变量
 load_dotenv()
 
 # 从 langchain 的 hub 拉取 ReAct 的提示模版
@@ -39,13 +31,13 @@ tools = [
 # 构建ReAct代理
 agent = create_react_agent(llm, tools, prompt)
 
-# 创建代理执行器并传入代理和工具，确保verbose=True
+# 创建代理执行器并传入代理和工具，并输出运行日志
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
 # 调用代理执行器，传入输入数据
-print("第一次运行的结果：")
-agent_executor.invoke({"input": "当前Agent最新研究进展是什么?"})
-
+response = agent_executor.invoke({"input": "新出的游戏《文明7》，怎么用领袖伊莎贝拉过神级难度？"})
+print("输出结果：")
+print(response["output"])
 
 
 
